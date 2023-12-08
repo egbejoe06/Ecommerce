3a38140988ef43b93e90ae0b7fdc96a89c63fc78
Egbeleke Jonathan <bunkumijoe@gmail.com>
2023-12-08 12:19:16 -0800

    update

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -102,7 +102,7 @@
                 <span class="cm52">${{ Total1.toFixed(2) }}</span>
               </div>
             </div>
-            <div class="cm6">
+            <div v-if="checkedout" @click="checkout()" class="cm6">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="23"
@@ -119,6 +119,51 @@
                 />
               </svg>
               <span>Checkout</span>
+            </div>
+            <div v-else class="cm6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="24"
+                viewBox="0 0 25 24"
+                fill="none"
+              >
+                <path
+                  d="M12.5 2V6"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M14.5 4L10.5 4"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M4.5 10V16.7639C4.5 17.5215 4.928 18.214 5.60557 18.5528L11.6056 21.5528C12.1686 21.8343 12.8314 21.8343 13.3944 21.5528L19.3944 18.5528C20.072 18.214 20.5 17.5215 20.5 16.7639V10"
+                  stroke="white"
+                />
+                <path
+                  d="M18.5 5L20.5 6L12.5 10L4.5 6L6.5 5"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M20.5 6L12.5 10L14.5 13L22.5 9L20.5 6Z"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M4.5 6L12.5 10L10.5 13L2.5 9L4.5 6Z"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <span>Shop now</span>
             </div>
           </div>
         </div>
@@ -163,6 +208,7 @@ export default {
     return {
       discount: false,
       gift: false,
+      checkedout: true,
     };
   },
   components: { Header, Footer },
@@ -195,6 +241,9 @@ export default {
     ...mapGetters("product", ["products", "isFavorite", "Images", "favoriteProducts"]),
   },
   methods: {
+    checkout() {
+      this.checkedout = !this.checkedout;
+    },
     addCart(product) {
       const isProductInCart = this.carts.some((item) => item.id === product.id);
       const Quantity = 1;
